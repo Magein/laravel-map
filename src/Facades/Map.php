799381c307c46d7ca228a85fc13605cf7b60da54
小马哥 <1154699897@qq.com>
2022-03-30 15:22:20 +0800

    update

--- src/Facades/Map.php
+++ src/Facades/Map.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Facade;
 use Magein\Map\Lib\Location;
 
 /**
- * @method static Location address($params)
- * @method static array location($params)
+ * @method static array address($params)
+ * @method static Location location($params)
  * @method static array ip($params)
  */
 class Map extends Facade
